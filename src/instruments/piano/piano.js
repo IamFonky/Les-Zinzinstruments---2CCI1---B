e21788da2802a9b292feb27922b0faca927088fa
Pierre-Benjamin Monaco <imfonky@gmail.com>
2026-09-22 10:41:44 +0200

    Correction du piano

--- src/instruments/piano/piano.js
+++ src/instruments/piano/piano.js
@@ -3,7 +3,7 @@ import { playTone } from "../../js/audio.js";
 export const id = "piano";
 
 export function play() {
-  //playTone(261.63, "triangle", 1.2, 0.4);
+  playTone(261.63, "triangle", 1.2, 0.4);
   playTone(329.63, "triangle", 1.2, 0.3);
   playTone(392, "triangle", 1.2, 0.25);
 }
